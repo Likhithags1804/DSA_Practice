40ac66be6136098ab983ddedb3a749a9fca451d7
LIKHITHA G S <likithags2004@gmail.com>
2025-06-19 13:42:53 +0530

    ZerosToend

--- Arrays/MoveAllZerosToEnd.java
+++ Arrays/MoveAllZerosToEnd.java
@@ -1,5 +1,5 @@
-
-/*Input: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
+package Arrays;
+/*nput: arr[] = [1, 2, 0, 4, 3, 0, 5, 0]
 Output: arr[] = [1, 2, 4, 3, 5, 0, 0, 0]
 Explanation: There are three 0s that are moved to the end.
 
@@ -9,33 +9,7 @@ Explanation: No change in array as there are no 0s.
 
 Input: arr[] = [0, 0]
 Output: arr[] = [0, 0]
-Explanation: No change in array as there are all 0s.
-say count to track where the next non-zero element should be placed. However, on encountering a non-zero element, instead of directly placing the non-zero element at arr[count], we will swap the non-zero element with arr[count]. 
-This will ensure that if there is any zero present at arr[count], it is pushed towards the end of array and is not overwritten. */
-class MoveAllZerosToEnd{
-    // Function which pushes all zeros to end of array
-    public static  void pushzerostoend(int[] arr){
-        // Pointer to track the position for next non-zero element
-        int count = 0;
-         for (int i = 0; i < arr.length; i++) {
-
-            // If the current element is non-zero
-            if (arr[i] != 0) {
-        // Swap the current element with the 0 at index 'count'
-                int temp =arr[i];
-                arr[i]=arr[count];
-                arr[count]=temp;
-      // Move 'count' pointer to the next position
-                count++;
-    }
-
+Explanation: No change in array as there are all 0s. */
+public class MoveAllZerosToEnd {
+    
 }
-    }
-     public static void main(String[] args) {
-        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
-       pushzerostoend(arr);
-       //for each loop
-       for (int num : arr) {
-            System.out.print(num + " ");
-     }}
-    }
