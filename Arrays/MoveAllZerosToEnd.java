ffd54c3716c124d0eeacab36384c3a280982e36a
LIKHITHA G S <likithags2004@gmail.com>
2025-06-19 14:14:46 +0530

    Move all zeros to end

--- Arrays/MoveAllZerosToEnd.java
+++ Arrays/MoveAllZerosToEnd.java
@@ -9,7 +9,42 @@ Explanation: No change in array as there are no 0s.
 
 Input: arr[] = [0, 0]
 Output: arr[] = [0, 0]
-Explanation: No change in array as there are all 0s. */
+Explanation: No change in array as there are all 0s. 
+The idea is similar to the previous approach where we took a pointer, say count to track where the next non-zero element should be placed. However, on encountering a non-zero element, instead of directly placing the non-zero element at arr[count], we will swap the non-zero element with arr[count]. 
+This will ensure that if there is any zero present at arr[count], it is pushed towards the end of array and is not overwritten.
+Time Complexity: O(n), as we are traversing the array only once.
+Auxiliary Space: O(1)
+
+*/
 public class MoveAllZerosToEnd {
-    
+    // Function which pushes all zeros to end of array
+    static void pushZerosToEnd(int[] arr) {
+
+        // Pointer to track the position for next non-zero element
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            // If the current element is non-zero
+            if (arr[i] != 0) {
+
+                // Swap the current element with the 0 at index 'count'
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+
+                // Move 'count' pointer to the next position
+                count++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+        pushZerosToEnd(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }
+
